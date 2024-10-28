b2a5c2cb9969bfb6b70820b07efbe32f3f7ff851
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2024-10-28 08:12:19 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chrome Extensions Sidebar Zoom Test
+
+Minimum reproduction of Chrome being unable to send zoom events in the extension sidepanel
---
+++ pinch.js
@@ -0,0 +1,4 @@
+var div = document.getElementById("the-div");
+div.addEventListener("wheel", function (event) {
+  console.log("wheel", event);
+});
